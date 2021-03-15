c2e2eacd55bf2f83305d30f27507388718d03e76
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-03-15 14:29:41 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -12,6 +12,7 @@ console.log(secretNumber);
 
 //Score Changes
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value); //JS will call this function when the event happens. We will not be manually calling this function in the event listener.
@@ -29,6 +30,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
     //When Guess is Too High
   } else if (guess > secretNumber && guess <= 20) {
     if (score > 1) {
