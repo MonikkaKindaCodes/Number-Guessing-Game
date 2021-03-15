86660b57e37326e82d84051db99fa074fd32282e
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-03-14 23:48:42 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -10,6 +10,9 @@
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
 
+//Score Changes
+let score = 20;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value); //JS will call this function when the event happens. We will not be manually calling this function in the event listener.
   console.log(guess, typeof guess);
@@ -23,9 +26,25 @@ document.querySelector('.check').addEventListener('click', function () {
       'You have the Secret Number! You Win!';
     document.querySelector('.number').textContent = secretNumber;
   } else if (guess > secretNumber && guess <= 20) {
-    document.querySelector('.message').textContent = 'Too High. Try Again';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High. Try Again';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'Game Over. Please Refresh to Restart Game';
+      document.querySelector('.score').textContent = 0;
+    }
   } else if (guess < secretNumber && guess <= 20) {
-    document.querySelector('.message').textContent = 'Too Low. Try Again';
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low. Try Again';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent =
+        'Game Over. Please Refresh to Restart Game';
+      document.querySelector('.score').textContent = 0;
+    }
   } else {
     document.querySelector('.message').textContent =
       'Please Enter A Number between 1 and 20';
