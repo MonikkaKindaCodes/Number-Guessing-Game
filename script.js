b4614ad5b2d77036bc631f3f5bcade350fb10a35
Monikka Edgeston <59119237+MonikkaKindaCodes@users.noreply.github.com>
2021-03-16 14:42:22 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 'use strict';
-
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 10;
-// document.querySelector('.guess').value = 45;
-
-// //Lines above are examples of DOM manipulation.
-
 //Setting Secret Number
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 console.log(secretNumber);
@@ -14,19 +7,24 @@ console.log(secretNumber);
 let score = 20;
 let highScore = 0;
 
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+  return message;
+}
+
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value); //JS will call this function when the event happens. We will not be manually calling this function in the event listener.
   console.log(guess, typeof guess);
 
   if (!guess || guess < 0 || guess > 20) {
     //You will get zero. Zero is a falsy value. Will give a boolean or false
-    document.querySelector('.message').textContent =
-      'Invalid Entry. Please Try Again'; //This changes the text in that area.
+    displayMessage('Invalid Entry. Please Try Again'); //This changes the text in that area.
 
     //When Player Wins the Game
   } else if (guess === secretNumber) {
-    document.querySelector('.message').textContent =
-      'You have the Secret Number! You Win!';
+
+    displayMessage('You have the Secret Number! You Win!');
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -39,12 +37,12 @@ document.querySelector('.check').addEventListener('click', function () {
   }
   else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = guess > secretNumber ? 'Too High, Try Again' : 'Too Low Try Again';
+      //Added Ternary Operator to get rid of the Too High, Too Low if/else blocks
+      displayMessage(guess > secretNumber ? 'Too High, Try Again' : 'Too Low, Try Again');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent =
-        'Game Over. Please Select Again to Restart Game';
+      displayMessage('Game Over. Please Select Again to Restart Game');
       document.querySelector('.score').textContent = 0;
     }
   }
@@ -57,6 +55,6 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('body').style.backgroundColor = '#222';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.number').textContent = '?';
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.guess').value = '';
 });
